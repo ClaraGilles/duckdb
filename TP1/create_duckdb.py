412b86d0bc781d5be035204cb4046e621ae203ae
ClaraGilles <clara.gilles.pro@gmail.com>
2025-06-03 09:53:43 +0200

    create insert pragma

--- TP1/create_duckdb.py
+++ TP1/create_duckdb.py
@@ -1,44 +1,73 @@
 import duckdb as dd
 
-con = dd.connect('taxi.db')
+# Connexion à la base
+con = dd.connect('../outputs/taxi.db')
 
-con.execute('''
-CREATE OR REPLACE TABLE taxi_trips (
-    VendorID INTEGER,
-    tpep_pickup_datetime TIMESTAMP,
-    tpep_dropoff_datetime TIMESTAMP,
-    passenger_count INTEGER,
-    trip_distance DOUBLE,
-    RatecodeID INTEGER,
-    store_and_fwd_flag VARCHAR,
-    PULocationID INTEGER,
-    DOLocationID INTEGER,
-    payment_type INTEGER,
-    fare_amount DOUBLE,
-    extra DOUBLE,
-    mta_tax DOUBLE,
-    tip_amount DOUBLE,
-    tolls_amount DOUBLE,
-    improvement_surcharge DOUBLE,
-    total_amount DOUBLE,
-    congestion_surcharge DOUBLE
-);
+def create_insert_table(file_name, table_name):
+    
+    if file_name == "data/taxi_data_csv.csv":
+        read_type = "read_csv_auto"
+    else:
+        read_type = "read_parquet"
+    
+    con.execute("PRAGMA profiling_mode='detailed'")
+    con.execute(f"PRAGMA profiling_output='../outputs/{table_name}.txt'")
+    con.execute("PRAGMA enable_profiling")
+    
+    con.execute(f'''
+        CREATE OR REPLACE TABLE {table_name} (
+            VendorID INTEGER,
+            tpep_pickup_datetime TIMESTAMP,
+            tpep_dropoff_datetime TIMESTAMP,
+            passenger_count INTEGER,
+            trip_distance DOUBLE,
+            RatecodeID INTEGER,
+            store_and_fwd_flag VARCHAR,
+            PULocationID INTEGER,
+            DOLocationID INTEGER,
+            payment_type INTEGER,
+            fare_amount DOUBLE,
+            extra DOUBLE,
+            mta_tax DOUBLE,
+            tip_amount DOUBLE,
+            tolls_amount DOUBLE,
+            improvement_surcharge DOUBLE,
+            total_amount DOUBLE,
+            congestion_surcharge DOUBLE
+        );
+    ''')
+
+    con.sql(f'''
+        INSERT INTO {table_name} (
+            VendorID, tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count,
+            trip_distance, RatecodeID, store_and_fwd_flag, PULocationID,
+            DOLocationID, payment_type, fare_amount, extra, mta_tax,
+            tip_amount, tolls_amount, improvement_surcharge, total_amount, congestion_surcharge
+        )
+        SELECT 
+            VendorID, tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count,
+            trip_distance, RatecodeID, store_and_fwd_flag, PULocationID,
+            DOLocationID, payment_type, fare_amount, extra, mta_tax,
+            tip_amount, tolls_amount, improvement_surcharge, total_amount, congestion_surcharge
+        FROM {read_type}('{file_name}')
+    ''')
+
+    print(con.sql(f'''
+        SELECT * FROM {table_name} LIMIT 5
+    '''))
+    
+# Exemple d’utilisation CSV
+create_insert_table('data/taxi_data_csv.csv', 'table_taxi_trips_csv')
+
+# Export d'une des tables
+con.sql('''
+    COPY table_taxi_trips_csv TO 'data/taxi_data_parquet.parquet' (FORMAT 'parquet'); 
 ''')
 
 con.sql('''
-    INSERT INTO taxi_trips (
-        VendorID, tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count,
-        trip_distance, RatecodeID, store_and_fwd_flag, PULocationID,
-        DOLocationID, payment_type, fare_amount, extra, mta_tax,
-        tip_amount, tolls_amount, improvement_surcharge, total_amount, congestion_surcharge
-    )
-    (SELECT 
-        VendorID, tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count,
-        trip_distance, RatecodeID, store_and_fwd_flag, PULocationID,
-        DOLocationID, payment_type, fare_amount, extra, mta_tax,
-        tip_amount, tolls_amount, improvement_surcharge, total_amount, congestion_surcharge
-    FROM "data/yellow_tripdata_2019-01.csv")
+    COPY table_taxi_trips_csv TO '../outputs/taxi_data_parquet.parquet' (FORMAT 'parquet'); 
 ''')
 
-print(con.sql('SHOW ALL TABLES'))
-print(con.sql('SELECT * FROM taxi_trips LIMIT 5;'))
+# Exemple d’utilisation Parquet
+create_insert_table('data/taxi_data_parquet.parquet', 'table_taxi_trips_parquet')
+
